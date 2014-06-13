5c90e8b86adeb09e75104dfa1d27358f13013876
wout <wout@impinc.co.uk>
2014-06-13 15:08:54 +0200

    Created the possibility to create various instances

--- trustfeed.js
+++ trustfeed.js
@@ -1,12 +1,13 @@
-// trustfeed.js 0.1.1 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
+// trustfeed.js 0.1.2 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
 
 var Trustfeed = this.Trustfeed = {
-  // Settings
-  load: function(options) {
-    var option, script, protocol
-
-    // Define default settings
-    this.settings = {
+  // Initialize status
+  status:     'waiting'
+  // Initialize instances store
+, instances: []
+  // Initialize defaults
+, defaults: function() {
+    return {
       class:         'trustfeed'
     , template:      '<article class="trustfeed-entry"><h3>{{title}}</h3><div class="stars tf-stars-{{stars}} tf-stars-{{starSize}}"></div><p class="content">{{content}}{{more}}</p><p class="author">{{name}}</p></article>'
     , readMore:      '<a href="{{url}}" target="_blank">Read more</a>'
@@ -16,63 +17,100 @@ var Trustfeed = this.Trustfeed = {
     , offset:        0
     , reverse:       true
     }
+  }
+  // Load instance
+, load: function(options) {
+    var settings
+      , option
+
+    // Get fresh defaults object
+    settings = this.defaults()
+
+    // Store settings
+    this.instances.push(settings)
 
     // Merge custom settings
     for (option in options)
-      this.settings[option] = options[option]
+      settings[option] = options[option]
 
-    // Determine protocol
-    protocol = window.location.protocol == 'https:' ? 'https:' : 'http:'
+    if (this.status == 'waiting') {
+      // Set loading status
+      this.status = 'loading'
 
-    // Load data
-    script = document.createElement('script')
-    script.src = this.interpolate('{{protocol}}//{{subdomain}}.trustpilot.com/tpelements/{{clientId}}/f.jsonp', {
-      clientId:   this.settings.clientId
-    , protocol:   protocol
-    , subdomain:  protocol == 'http:' ? 's' : 'ssl'
-    })
-    document.getElementsByTagName('head')[0].appendChild(script)
+      // Determine protocol
+      var protocol = window.location.protocol == 'https:' ? 'https:' : 'http:'
+
+      // Load data
+      var script = document.createElement('script')
+      script.src = this.interpolate('{{protocol}}//{{subdomain}}.trustpilot.com/tpelements/{{clientId}}/f.jsonp', {
+        clientId:   settings.clientId
+      , protocol:   protocol
+      , subdomain:  protocol == 'http:' ? 's' : 'ssl'
+      })
+      document.getElementsByTagName('head')[0].appendChild(script)
+    }
+
+    // Wait for data to arrive
+    function proceed() {
+      if (Trustfeed.status == 'loaded')
+        Trustfeed.run(Trustfeed.data)
+      else
+        setTimeout(proceed, 50)
+    }
+    
+    proceed()
 
   }
-  // Run result
-, run: function(data) {
-    var i, elements
-
-    // Store data
+  // Receive data
+, receive: function(data) {
     this.data = data
-    if (this.settings.reverse === true)
-      this.data.Reviews.reverse()
-    this.data.Reviews = this.data.Reviews.slice(this.settings.offset, this.settings.limit + this.settings.offset)
-  
-    // Get all elements
-    elements = document.getElementsByClassName(this.settings.class)
-  
-    // Iterate all elements
-    for (i = elements.length - 1; i >= 0; i--)
-      this.build(elements[i])
+    this.status = 'loaded'
+  }
+  // Run result
+, run: function() {
+    var i, settings, elements, data
+
+    // Run every instance
+    while (settings = this.instances.shift()) {
+      // Get a fresh copy of data
+      data = JSON.parse(JSON.stringify(this.data))
+
+      // Manipulate data
+      if (settings.reverse === true)
+        data.Reviews.reverse()
+
+      data.Reviews = data.Reviews.slice(settings.offset, settings.limit + settings.offset)
+      
+      // Get all elements
+      elements = document.getElementsByClassName(settings.class)
+      
+      // Iterate all elements
+      for (i = elements.length - 1; i >= 0; i--)
+        this.build(elements[i], settings, data)
+    }
 
   }
   // Build element
-, build: function(element) {
+, build: function(element, settings, data) {
     var i, review
 
     // Clear existing content
     element.innerHTML = ''
 
     // Add reviews
-    for (i = 0; i < this.data.Reviews.length; i++)
-      element.innerHTML = element.innerHTML + this.interpolate(this.settings.template, this.review(this.data.Reviews[i]))
+    for (i = 0; i < data.Reviews.length; i++)
+      element.innerHTML = element.innerHTML + this.interpolate(settings.template, this.review(data.Reviews[i], settings))
     
   }
   // Prepare data
-, review: function(review) {
+, review: function(review, settings) {
     var content = review.Content
       , more    = ''
 
     // Truncate content if required
-    if (content.length > this.settings.maxCharacters) {
-      content = content.substring(0, this.settings.maxCharacters) + '... '
-      more    = this.interpolate(this.settings.readMore, { url: review.Url })
+    if (content.length > settings.maxCharacters) {
+      content = content.substring(0, settings.maxCharacters) + '... '
+      more    = this.interpolate(settings.readMore, { url: review.Url })
     }
 
     return {
@@ -82,11 +120,11 @@ var Trustfeed = this.Trustfeed = {
     , more:     more
     , score:    review.TrustScore.Score
     , stars:    review.TrustScore.Stars
-    , image:    review.TrustScore.StarsImageUrls[this.settings.starSize]
+    , image:    review.TrustScore.StarsImageUrls[settings.starSize]
     , name:     review.User.Name
     , date:     review.Created.HumanDate
     , datetime: review.Created.Human
-    , starSize: this.settings.starSize
+    , starSize: settings.starSize
     }
   }
   // Interpoate string
@@ -101,5 +139,5 @@ var Trustfeed = this.Trustfeed = {
 
 // Add global callback
 function trustpilot_jsonp_callback(data) {
-  Trustfeed.run(data)
+  Trustfeed.receive(data)
 }
